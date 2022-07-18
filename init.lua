67c36b4144d993207c3b942e714a508f326bb2b0
Felipe Ortega <felipeortegabustamante@gmail.com>
2022-07-17 21:37:28 -0400

    refactor: simplifiy things. sort and put some comments

--- init.lua
+++ init.lua
@@ -1,16 +1,45 @@
-pins = {5, 4, 0, 2};
-state = gpio.HIGH
-uptime = 0
-max_uptime = 86400
-switch_time = 64800
+-- constants
+
+PINS = {5, 4, 0, 2};
+MAX_UPTIME = 86400
+ON_TIME = 61200
+OFF_TIME = 39600
+STATE_ON = gpio.LOW
+STATE_OFF = gpio.HIGH
+
+-- variables
+
+local uptime = 0
+local states = {STATE_OFF, STATE_OFF, STATE_OFF, STATE_OFF}
+
+-- logic
+
+function calcStates()
+    local state
+    if ((uptime < OFF_TIME) or (uptime > ON_TIME)) then
+        state = STATE_ON
+    else
+        state = STATE_OFF
+    end
+
+    states[1] = state
+    states[2] = state
+    states[3] = state
+    states[4] = state
+end
+
+-- system io
 
 function writeState(pin)
-    gpio.write(pin, state)
+    print(PINS[pin], states[pin])
+    gpio.write(PINS[pin], states[pin])
 end
 
 function setupPin(pin)
-    gpio.mode(pin, gpio.OUTPUT)
+    gpio.mode(PINS[pin], gpio.OUTPUT)
 end
+
+-- Wifi
 
 function setupWifi()
     wifi.setmode(wifi.SOFTAP)
@@ -21,6 +50,8 @@ function setupWifi()
     })
 end
 
+-- api
+
 function buildResponse(json)
     return string.format("HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nContent-Length: %d\r\n\r\n%s",
         string.len(json), json)
@@ -30,18 +61,33 @@ function setupApi()
     srv = net.createServer(net.TCP)
     srv:listen(80, function(conn)
         conn:on("receive", function(conn, payload)
-            new_uptime = payload: match "GET /([0-9]+) HTTP/"
-            if(new_uptime ~= nil)
-            then
-                print("new uptime:", new_uptime)
+            new_uptime = payload:match "GET /([0-9]+) HTTP/"
+            if (new_uptime ~= nil) then
                 uptime = tonumber(new_uptime)
             end
-            
+
             response = buildResponse(string.format("{\"uptime\": %d}", uptime))
             conn:send(response)
         end)
     end)
 end
+
+-- timer
+
+function tick()
+    calcStates()
+    foreach(writeState)
+
+    uptime = (uptime + 1) % MAX_UPTIME
+end
+
+function startTimer()
+    timer = tmr.create()
+    timer:register(1000, tmr.ALARM_AUTO, tick)
+    timer:start()
+end
+
+-- utils
 
 function foreach(f)
     for i = 1, 4, 1 do
@@ -49,28 +95,14 @@ function foreach(f)
     end
 end
 
-function switchState()
-    turnon = (uptime < switch_time)
-
-    if (turnon) then
-        state = gpio.HIGH
-    else
-        state = gpio.LOW
-    end
-    foreach(writeState)
-
-    uptime = (uptime + 1) % max_uptime
-end
-
 function setup()
     setupWifi()
     setupApi()
     foreach(setupPin)
     foreach(writeState)
-
-    timer = tmr.create()
-    timer:register(1000, tmr.ALARM_AUTO, switchState)
-    timer:start()
 end
 
+-- setup and start
+
 setup()
+startTimer()
